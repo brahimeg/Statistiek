9ad2e0132fc3163eef08d8af20773f5f060bd913
Marilva <37208678+Marilva@users.noreply.github.com>
2018-03-23 11:31:48 +0100

    23-3
    .

--- Code 4.R
+++ Code 4.R
@@ -1,4 +1,4 @@
-setwd("C:\\Users\\Marilva\\Documents\\Statistiek\\Eindopdracht")
+setwd("C:\\Users\\Marilva\\Documents\\GitHub\\Statistiek")
 load("allData2018(1).Rdata")
 WoordenlijstTraining <- read.csv("trainingList.csv", header = TRUE)
 
@@ -21,10 +21,41 @@ LengteHetzelfde <- function(woord, lijst)
   lengte <- nchar(woord)
   KeuzeWoorden <- lijst[lijst$nrLetters == lengte, ]
   Nieuwwoord <- KeuzeWoorden[sample(nrow(KeuzeWoorden), 1), ]    
-  as.character(Nieuwwoord[[1,2]])
+  as.character(Nieuwwoord[1,2])
 }
 
 LengteHetzelfde("kaas", WoordenlijstTraining)
 
+LetterVeranderen <- function(woord, lijst)
+{
+  for (r in 1:nrow(lijst))
+  {
+  verschil <- Reduce(setdiff, strsplit(c(woord, lijst$eandowf), split = ""))
+  hoeveel <- nchar(verschil)
+  if {
+    
+  }
+  }
+}
 
+
+
+
+eersteLetterHetzelfde <- function(woord, lijst)
+{
+WoordenlijstTraining$beginLetter <- NA
+header <- 
+for (r in 1:nrow(lijst))
+  {
+  string <- lijst[r,]$eandowf
+  lijst[r,]$beginLetter <- substr(string, 1, 1)
+}
+
+begin <- substr(woord, 1, 1)
+Keuzes <- lijst[lijst$beginLetter == begin, ]
+output <- Keuzes[sample(nrow(Keuzes), 1), ]
+as.character(output[1,2])
+}
+
+eersteLetterHetzelfde('aardbei', WoordenlijstTraining)
 
